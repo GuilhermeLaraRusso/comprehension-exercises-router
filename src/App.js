60a88db16d858cde9fc118796cfa4d8107044909
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-08-02 13:34:59 -0300

    exercicio pronto. necessario ajustar css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import {BrowserRouter} from 'react-router-dom';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import About from './components/About';
+import Users from './components/Users';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Route exact path="/" component= {Home} />
+        <Route path="/about" component={About} />
+        <Route path="/users/:id" render={(props) => <Users {...props} greetingMessage={"Good Morning"} /> } />
+      </BrowserRouter>
     );
   }
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Users extends Component {
   render() {
+    const { greetingMessage } = this.props;
+    const { id } = this.props.match.params;
     return (
       <div>
         <h2>Users</h2>
-        <p> My awesome Users component </p>
+        <p> { greetingMessage }, My awesome Users component { id } </p>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
       </div>
     );
   }
